aff0a29a60159e1be652a28800fae73cb29923b3
MichaelSNelson <imagescientistwebsite@gmail.com>
2026-04-27 16:19:40 -0500

    Bump version to 0.2.5 for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 qupathExtension {
     name = "qupath-extension-cell-analysis-tools"
     group = "io.github.uw-loci"
-    version = "0.2.4"
+    version = "0.2.5"
     description = "QP-CAT: Cell Analysis Tools for QuPath. Python-powered clustering, phenotyping, classification, and spatial analysis for multiplexed imaging data."
     automaticModule = "io.github.uw-loci.extension.qpcat"
 }
